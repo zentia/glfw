59f39082fd6d5a3c369eca8d88f5716239acdc0a
zentia <363476658@qq.com>
2025-04-02 20:17:40 +0800

    xmake

---
+++ xmake.lua
@@ -0,0 +1,31 @@
+target("glfw")
+    set_kind("$(kind)")
+    add_includedirs("include", {public = true})
+    add_headerfiles(
+        "src/win32_platform.h",
+        "src/win32_joystick.h"
+    )
+    add_files(
+        "src/win32_init.c", 
+        "src/win32_joystick.c", 
+        "src/win32_monitor.c", 
+        "src/win32_window.c", 
+        "src/win32_thread.c",
+        "src/win32_time.c",
+        "src/win32_module.c",
+        "src/wgl_context.c",
+        "src/window.c",
+        "src/init.c",
+        "src/null_init.c",
+        "src/null_joystick.c",
+        "src/input.c",
+        "src/context.c",
+        "src/platform.c",
+        "src/monitor.c",
+        "src/null_monitor.c",
+        "src/vulkan.c",
+        "src/egl_context.c",
+        "src/osmesa_context.c",
+        "src/null_window.c"
+    )
+    add_defines("_GLFW_WIN32")
